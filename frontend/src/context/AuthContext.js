1956fa28f70e776efe902a05cab092c3f2bb3775
Akshay Sharma <akshaysharmaay@gmail.com>
2022-09-03 20:19:52 +0530

    Created Auth Context

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,26 @@
+import { createContext } from "react";
+
+export const AuthContext = createContext();
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { user: action.payload };
+    case "LOGOUT":
+      return { user: null };
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, { childrenuser: null });
+
+  connsole.log("AuthContext state: ", state);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
